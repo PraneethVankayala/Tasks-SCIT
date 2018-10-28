a52a5f1aef555755928fbd6410b8951bf2f85d5f
PraneethVankayala <bspraneethvankayala@gmail.com>
2018-10-29 02:35:20 +0530

    Histogram using poisson function

--- poisson.py
+++ poisson.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import csv
 ar=np.random.poisson(30,1000)
 
 bins=plt.hist(ar, 14, normed=True)
@@ -12,3 +12,10 @@ plt.grid(axis='y',alpha=0.75)
 plt.title("Poisson Distribution")
 plt.text(41,0.06,r'$lamda=30$')
 plt.show()
+
+with open('poisson.csv','w') as csvfile:
+	fieldnames=['points']
+	writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+	writer.writeheader()
+	for i in ar:
+		writer.writerow({'points': i})
