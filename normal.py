7b55787fc6624776b69591c19496fbab83b08e0c
PraneethVankayala <bspraneethvankayala@gmail.com>
2018-10-29 00:44:18 +0530

    Histogram using normal function

---
+++ normal.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+s=np.random.normal(loc=0.0,scale=1.0,size=1000)
+
+import matplotlib.pyplot as plt
+
+plt.hist(s,14,density=True)
+plt.title("Normal Graph")
+plt.grid(axis='x',alpha=0.75)
+plt.grid(axis='y',alpha=0.75)
+plt.show();
