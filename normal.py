37847a0c5f554a77f10bd0f8fc13281980f5b045
PraneethVankayala <bspraneethvankayala@gmail.com>
2018-10-29 02:34:45 +0530

    Histogram using normal function

--- normal.py
+++ normal.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import csv
 s=np.random.normal(loc=30.0,scale=1.0,size=1000)
 
 import matplotlib.pyplot as plt
@@ -9,3 +9,10 @@ plt.title("Normal Graph")
 plt.grid(axis='x',alpha=0.75)
 plt.grid(axis='y',alpha=0.75)
 plt.show();
+
+with open('normal.csv','w') as csvfile:
+	fieldnames=['points']
+	writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+	writer.writeheader()
+	for i in s:
+		writer.writerow({'points': i})
