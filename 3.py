954cc4c0618cbbb931d256f6c41a9fe4400e2ac0
PraneethVankayala <bspraneethvankayala@gmail.com>
2018-10-29 01:35:43 +0530

    Averaged the Histograms

---
+++ 3.py
@@ -0,0 +1,23 @@
+from poisson import ar
+from normal import s
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+lp = []
+ln = []
+totallist = []
+for i in range(100):
+	sp=random.choice(ar)
+	lp.append(sp)
+for i in range(100):
+	n=random.choice(s)
+	ln.append(n)
+totallist=lp+ln
+print totallist
+totallist=np.array([lp,ln])
+totallist=np.average(totallist,axis=0)
+bins = plt.hist(totallist,14,density=True)
+plt.grid(axis='x',alpha=0.75)
+plt.grid(axis='y',alpha=0.75)
+plt.show();
